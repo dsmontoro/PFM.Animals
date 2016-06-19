53a6a9fabd746ff3c14d71b313d84e0c24bdbe72
dsmontoro <dsm@alumnos.upm.es>
2016-06-19 08:37:51 +0200

    Login

--- src/main/java/business/api/Uris.java
+++ src/main/java/business/api/Uris.java
@@ -13,8 +13,6 @@ public class Uris {
     public static final String USERS = "/users";
     
     public static final String ASSOCIATIONS = "/associations";
-
-    public static final String TOKENS = "/tokens";
     
     public static final String LOGIN = "/login";
     
