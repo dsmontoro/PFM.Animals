64d61536176c86e6fe7fe150d9b4a3794b461439
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2016-06-26 14:54:17 +0200

    Se restuara el multipart para permitir subidas de imagenes

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,20 +1,31 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {ResourceNames.REST_API, ResourceNames.CONTROLLERS, ResourceNames.DAOS, ResourceNames.SERVICES, ResourceNames.WRAPPERS})
+@ComponentScan(basePackages = {ResourceNames.REST_API, ResourceNames.CONTROLLERS, ResourceNames.DAOS, ResourceNames.SERVICES,
+        ResourceNames.WRAPPERS})
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     // CORS
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").maxAge(3600);
-    }       
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(500000000);
+        return multipartResolver;
+    }
 
 }
