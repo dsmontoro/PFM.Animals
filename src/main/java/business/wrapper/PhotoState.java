7466c8954e50f165345f8ae883b11de85883a16f
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2016-06-27 11:58:56 +0200

    Update recurso de imagenes

--- src/main/java/business/wrapper/PhotoState.java
+++ src/main/java/business/wrapper/PhotoState.java
@@ -3,37 +3,43 @@ package business.wrapper;
 import data.entities.Photo;
 
 public class PhotoState {
-	
-	private int id;
-	
-	private String name;
-	
-	public PhotoState() {
-		
-	}
-	
-	public PhotoState(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
-	public PhotoState(Photo photo) {
-		this(photo.getId(),photo.getName());
-	}
 
-	public int getId() {
-		return id;
-	}
+    private int id;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    private String name;
 
-	public String getName() {
-		return name;
-	}
+    public PhotoState() {
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    }
+
+    public PhotoState(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public PhotoState(Photo photo) {
+        this(photo.getId(), photo.getName());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PhotoState [id=" + id + ", name=" + name + "]";
+    }
+
 }
